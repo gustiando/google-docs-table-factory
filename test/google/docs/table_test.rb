db53d49d0387452706103f053b07ad8d914db1ee
Gustavo Matias <gustavo@matias.com>
2019-12-28 14:24:27 -0800

    Create insert_table and insert_text requests given table array

--- test/google/docs/table_test.rb
+++ test/google/docs/table_test.rb
@@ -5,7 +5,40 @@ class Google::Docs::TableTest < Minitest::Test
     refute_nil ::Google::Docs::Table::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_has_correct_insert_text_requests_when_table_data_has_all_cells_filled
+    table_data = [['A1', 'B1'], ['A2', 'B2']]
+    table_index = 0
+
+    actual = ::Google::Docs::Table.create_insert_table_request(table_data, table_index)
+
+    assert_equal(actual,
+      [
+        { insert_table: { columns: 2, rows: 2, location: { index: 0 } } },
+        { insert_text: { location: { index: 11 }, text: 'B2' } },
+        { insert_text: { location: { index: 9 }, text: 'A2' } },
+        { insert_text: { location: { index: 6 }, text: 'B1' } },
+        { insert_text: { location: { index: 4 }, text: 'A1' } },
+      ],
+    )
+  end
+
+  def test_it_has_correct_insert_text_requests_when_table_data_has_some_empty_cells
+    table_data = [['A1', 'B1', 'C1'], [nil, 'B2', nil], ['A3', 'B3', 'C3']]
+    table_index = 0
+
+    actual = ::Google::Docs::Table.create_insert_table_request(table_data, table_index)
+
+    assert_equal(actual,
+      [
+        { insert_table: { columns: 3, rows: 3, location: { index: 0 } } },
+        { insert_text: { location: { index: 22 }, text: 'C3' } },
+        { insert_text: { location: { index: 20 }, text: 'B3' } },
+        { insert_text: { location: { index: 18 }, text: 'A3' } },
+        { insert_text: { location: { index: 13 }, text: 'B2' } },
+        { insert_text: { location: { index: 8 }, text: 'C1' } },
+        { insert_text: { location: { index: 6 }, text: 'B1' } },
+        { insert_text: { location: { index: 4 }, text: 'A1' } },
+      ],
+    )
   end
 end
