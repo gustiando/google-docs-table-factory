4861ef7b43bf0b064acec8c0e53895a5bee74b31
Gustavo Matias <gustavo@matias.com>
2019-12-28 13:15:44 -0800

    Add minitest-reporter for better test report formatting

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in google-docs-table.gemspec
 gemspec
+
+gem "minitest-reporters", "~> 1.4", group: :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "google/docs/table"
 
 require "minitest/autorun"
+require "minitest/reporters"
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
