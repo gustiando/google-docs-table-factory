b6249391d6a628138940b353bd79c5f433d2f8bf
Gustavo Matias <gustavo@matias.com>
2019-12-29 18:20:47 -0800

    Update index in test

--- test/google/api/docs_v1/table_factory_test.rb
+++ test/google/api/docs_v1/table_factory_test.rb
@@ -7,38 +7,35 @@ class Google::Api::DocsV1::TableFactoryTest < Minitest::Test
 
   def test_it_has_correct_insert_text_requests_when_table_data_has_all_cells_filled
     table_data = [['A1', 'B1'], ['A2', 'B2']]
-    table_index = 0
 
-
-    actual = ::Google::Api::DocsV1::TableFactory.insert_table_request(index: 0, table_data: table_data)
+    actual = ::Google::Api::DocsV1::TableFactory.insert_table_request(index: 1, table_data: table_data)
 
     assert_equal(actual,
       [
-        { insert_table: { columns: 2, rows: 2, location: { index: 0 } } },
-        { insert_text: { location: { index: 11 }, text: 'B2' } },
-        { insert_text: { location: { index: 9 }, text: 'A2' } },
-        { insert_text: { location: { index: 6 }, text: 'B1' } },
-        { insert_text: { location: { index: 4 }, text: 'A1' } },
+        { insert_table: { columns: 2, rows: 2, location: { index: 1 } } },
+        { insert_text: { location: { index: 12 }, text: 'B2' } },
+        { insert_text: { location: { index: 10 }, text: 'A2' } },
+        { insert_text: { location: { index: 7 }, text: 'B1' } },
+        { insert_text: { location: { index: 5 }, text: 'A1' } },
       ],
     )
   end
 
   def test_it_has_correct_insert_text_requests_when_table_data_has_some_empty_cells
     table_data = [['A1', 'B1', 'C1'], [nil, 'B2', nil], ['A3', 'B3', 'C3']]
-    table_index = 0
 
-    actual = ::Google::Api::DocsV1::TableFactory.insert_table_request(index: 0, table_data: table_data)
+    actual = ::Google::Api::DocsV1::TableFactory.insert_table_request(index: 1, table_data: table_data)
 
     assert_equal(actual,
       [
-        { insert_table: { columns: 3, rows: 3, location: { index: 0 } } },
-        { insert_text: { location: { index: 22 }, text: 'C3' } },
-        { insert_text: { location: { index: 20 }, text: 'B3' } },
-        { insert_text: { location: { index: 18 }, text: 'A3' } },
-        { insert_text: { location: { index: 13 }, text: 'B2' } },
-        { insert_text: { location: { index: 8 }, text: 'C1' } },
-        { insert_text: { location: { index: 6 }, text: 'B1' } },
-        { insert_text: { location: { index: 4 }, text: 'A1' } },
+        { insert_table: { columns: 3, rows: 3, location: { index: 1 } } },
+        { insert_text: { location: { index: 23 }, text: 'C3' } },
+        { insert_text: { location: { index: 21 }, text: 'B3' } },
+        { insert_text: { location: { index: 19 }, text: 'A3' } },
+        { insert_text: { location: { index: 14 }, text: 'B2' } },
+        { insert_text: { location: { index: 9 }, text: 'C1' } },
+        { insert_text: { location: { index: 7 }, text: 'B1' } },
+        { insert_text: { location: { index: 5 }, text: 'A1' } },
       ],
     )
   end
